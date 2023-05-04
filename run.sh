a00d2a03c1fc91dda5e21fff19d399c55b096706
Joel Berger <joel.a.berger@gmail.com>
2023-05-04 09:57:39 -0500

    add loop counter and final update count output
    when the job unexpectedly finds no annotated secrets there is no output
    at all, making the user question whether it ran or what happened. By
    adding a counter and a final statement of the number of items updated
    the user can see that no items were updated and know that there was work
    attempted but nothing happened
    
    note that this involved moving the code around. I had to keep the last
    add loop counter and final update count output
    
    when the job unexpectedly finds no annotated secrets there is no output
    at all, making the user question whether it ran or what happened. By
    adding a counter and a final statement of the number of items updated
    the user can see that no items were updated and know that there was work
    attempted but nothing happened
    
    note that this involved moving the code around. I had to keep the last
    pipe step in the current shell rather than a subshell. see
    https://tclish.org/2019/02/14/the-lastpipe-maneuver/
    pipe step in the current shell rather than a subshell.
    see https://tclish.org/2019/02/14/the-lastpipe-maneuver/ for examples
    and lastpipe did not work in my mac's bash for testing so I went with
    the standard, even if it is slightly less readable

--- run.sh
+++ run.sh
@@ -2,19 +2,25 @@
 
 set -e
 
-kubectl get -A secrets -o json \
+count=0
+
+while IFS=$'\t' read -r name namespace annotation
+do
+  IFS="/"
+  set $annotation
+  AWS_ACCOUNT=$1
+  AWS_DEFAULT_REGION=$2
+  registry="$AWS_ACCOUNT.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
+  echo "updating $namespace/$name: $AWS_ACCOUNT $AWS_DEFAULT_REGION $registry"
+  password=$(aws ecr get-login-password --region $AWS_DEFAULT_REGION)
+  auth=$(echo "AWS:$password" | base64)
+  dockerconfig=$(jq -n ".auths[\"$registry\"].auth = \"$auth\"" | base64)
+  patch=$(jq -n ".data[\".dockerconfigjson\"] = \"$dockerconfig\"")
+  kubectl patch -n $namespace secret $name -p $patch
+  ((count++))
+done < <( \
+  kubectl get -A secrets -o json \
   | jq -r '.items[].metadata | select(.annotations | has("deft.com/kubeecr")) | [.name, .namespace, .annotations."deft.com/kubeecr"] | @tsv' \
-  | while IFS=$'\t' read -r name namespace annotation
-  do
-    IFS="/"
-    set $annotation
-    AWS_ACCOUNT=$1
-    AWS_DEFAULT_REGION=$2
-    registry="$AWS_ACCOUNT.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com"
-    echo "updating $namespace/$name: $AWS_ACCOUNT $AWS_DEFAULT_REGION $registry"
-    password=$(aws ecr get-login-password --region $AWS_DEFAULT_REGION)
-    auth=$(echo "AWS:$password" | base64)
-    dockerconfig=$(jq -n ".auths[\"$registry\"].auth = \"$auth\"" | base64)
-    patch=$(jq -n ".data[\".dockerconfigjson\"] = \"$dockerconfig\"")
-    kubectl patch -n $namespace secret $name -p $patch
-  done
+)
+
+echo "updates complete: $count secrets updated"
